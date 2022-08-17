905cabf071ee38fd9fbe097112d38b8357705b2f
Murali.r <iammurali.mng@gmail.com>
2022-08-17 20:41:08 +0530

    Update Utility.java

--- src/Utilities/Utility.java
+++ src/Utilities/Utility.java
@@ -1,11 +1,26 @@
 package Utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Utility 
 {
 	
-	public static void launchBrowser()
+	public static void main(String[] args) 
+	{
+		launchApplication();
+	}
+	
+	public static void launchApplication()
 	{
 		
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://imuraliraj.github.io/MrBeem/");
 	}
 
 }
