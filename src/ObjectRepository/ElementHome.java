ee7d2d35d252d63b6fd8cdee11197b3c73bd692f
Murali.r <iammurali.mng@gmail.com>
2022-08-28 23:55:50 +0530

    Update ElementHome.java

--- src/ObjectRepository/ElementHome.java
+++ src/ObjectRepository/ElementHome.java
@@ -1,5 +1,51 @@
 package ObjectRepository;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class ElementHome {
+
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement websiteName;
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement websiteInformation;
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement websiteSlogan;
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement dogs;
+		
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement foods;
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement medicines;
+
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement clothes;
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement signup;
+
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement login;
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement logout;
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement howMrBeemHelpQuestion;
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement howMrBeemHelpAnswer;	
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement howToBuyQuestion;	
+	
+	@FindBy(xpath="//*[@id='']")
+	public static WebElement howToBuyAnswer;	
 
 }
