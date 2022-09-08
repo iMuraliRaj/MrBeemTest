46a1424a19cad2a3c986705364ef84ca02f9e450
Murali.r <iammurali.mng@gmail.com>
2022-09-08 22:30:59 +0530

    click

--- src/Utilities/Utility.java
+++ src/Utilities/Utility.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
 
@@ -46,6 +47,11 @@ public class Utility
 		
 		FileHandler.copy(src, des);
 		
+	}
+	
+	public static void click(WebElement element)
+	{
+		element.click();
 	}
 
 }
