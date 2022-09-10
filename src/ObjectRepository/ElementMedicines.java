b508a1bc79a762d9aad9c698587768d2fbf5d4fb
Murali.r <iammurali.mng@gmail.com>
2022-09-10 15:21:00 +0530

    Update ElementMedicines.java

--- src/ObjectRepository/ElementMedicines.java
+++ src/ObjectRepository/ElementMedicines.java
@@ -1,5 +1,15 @@
 package ObjectRepository;
 
-public class ElementMedicines {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ElementMedicines
+{
+	
+	@FindBy(xpath="//*[@id=\"clotheslink\"]']")
+	public static WebElement medicines;
+
+
+	
 
 }
